7aed7b76a20ff187a11d16cda02cbc1c24fe5d58
KaabiTaoufik <taoufik_c10@hotmail.com>
2023-08-20 11:17:20 +0100

    Fix UI, add automapper and create VM for leave type

---
+++ LeaveManagement.web/Models/LeaveTypeVM.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Leave Description")]
+        public string? Description { get; set; }
+
+        [Display(Name = "Deafult Number Of Days")]
+        public int DefaultDays { get; set; }
+    }
+}
