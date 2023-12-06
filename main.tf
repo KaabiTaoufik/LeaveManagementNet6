40088820f8db2d41a662ea6d529b82f779a85a74
alabenhamouda <ala.benhamouda@credium.de>
2023-12-06 20:08:28 +0100

    fix connection string

--- main.tf
+++ main.tf
@@ -26,6 +26,24 @@ resource "azurerm_resource_group" "resource_group" {
   location = "westeurope"
 }
 
+resource "azurerm_mssql_server" "sql" {
+  name                         = "leave-management-sql-server"
+  resource_group_name          = azurerm_resource_group.resource_group.name
+  location                     = azurerm_resource_group.resource_group.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_login
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_mssql_database" "db" {
+  name           = "leave-management-sql-db"
+  server_id      = azurerm_mssql_server.sql.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  sku_name       = "S0"
+  zone_redundant = false
+}
+
 resource "azurerm_service_plan" "app_service_plan" {
   name                = "leave-management-app-service-plan"
   location            = azurerm_resource_group.resource_group.location
@@ -47,24 +65,6 @@ resource "azurerm_linux_web_app" "web_app" {
   connection_string {
     name  = "DefaultConnection"
     type  = "SQLAzure"
-    value = "Server=tcp:azurerm_mssql_server.sql.fully_qualified_domain_name;Database=azurerm_mssql_database.db.name;User ID=azurerm_mssql_server.sql.administrator_login;Password=azurerm_mssql_server.sql.administrator_login_password;Trusted_Connection=False;Encrypt=True;"
+    value = "Server=tcp:${azurerm_mssql_server.sql.fully_qualified_domain_name};Database=${azurerm_mssql_database.db.name};User ID=${azurerm_mssql_server.sql.administrator_login};Password=${azurerm_mssql_server.sql.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
   }
-}
-
-resource "azurerm_mssql_server" "sql" {
-  name                         = "leave-management-sql-server"
-  resource_group_name          = azurerm_resource_group.resource_group.name
-  location                     = azurerm_resource_group.resource_group.location
-  version                      = "12.0"
-  administrator_login          = var.sql_admin_login
-  administrator_login_password = var.sql_admin_password
-}
-
-resource "azurerm_mssql_database" "db" {
-  name           = "leave-management-sql-db"
-  server_id      = azurerm_mssql_server.sql.id
-  collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
-  sku_name       = "S0"
-  zone_redundant = false
 }
